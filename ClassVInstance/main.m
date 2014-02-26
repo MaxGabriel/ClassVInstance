231051463b84395465e6ccfd3573a73e71538f1d
Maximilian Tagher <feedback.tagher@gmail.com>
2014-02-26 08:45:02 -0800

    Initial Commit

---
+++ ClassVInstance/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ClassVInstance
+//
+//  Created by Maximilian Tagher on 2/26/14.
+//  Copyright (c) 2014 Tagher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
